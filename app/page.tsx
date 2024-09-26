3e23c1c212006cbe3b78f5857c0c3cac33aea869
RNanWP <renan92011@hotmail.com>
2024-09-26 20:25:25 -0300

    criando navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@nextui-org/button";
+import { HeadersAdapter } from "next/dist/server/web/spec-extension/adapters/headers";
+import Head from "next/head";
 import Image from "next/image";
+import Header from "./_components/Header"
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello</h2>
-      <Button color="primary">
-        Subscribe
-      </Button>
+    <div className="bg-[#cad3ff]">
+      {/*Header*/}
+      <Header/>
+
+      {/*Hero*/}
     </div>
   );
 }
