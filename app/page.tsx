bd2b2a0ccb9b3106fb883ae3b9ce97cf53bb37ce
RNanWP <renan92011@hotmail.com>
2024-09-26 20:48:02 -0300

    organizando o Hero e botoes

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { HeadersAdapter } from "next/dist/server/web/spec-extension/adapters/hea
 import Head from "next/head";
 import Image from "next/image";
 import Header from "./_components/Header"
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Header/>
 
       {/*Hero*/}
+      <Hero/>
     </div>
   );
 }
