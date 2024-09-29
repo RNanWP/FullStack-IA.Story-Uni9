b1812571e2b5c30451fec2f16f2919de7a55b0f8
RNanWP <renan92011@hotmail.com>
2024-09-29 17:25:27 -0300

    Adicionando atutenticação/loguin, google, fb

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,21 @@
 import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div>
+            <Image 
+            src={'/login.png'} 
+            alt='login' 
+            width={700} 
+            height={1000} 
+            className=' h-screen w-full' 
+            />
+        </div>
+        <div className='flex justify-center items-center h-screen order-first md:order-last bg-gray-50 p-6'>
+            <SignIn/>
+        </div>
+    </div>
+  )
 }
