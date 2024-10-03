34a69a8037f841578c2fea894f8a240fd48e9883
RNanWP <renan92011@hotmail.com>
2024-10-02 22:23:26 -0300

    Adicionando sombra e borda nos estilos de histórias selecionados

--- app/create-story/_components/StoryType.tsx
+++ app/create-story/_components/StoryType.tsx
@@ -30,14 +30,19 @@ function StoryType() {
         <div className='grid grid-cols-3 gap-5 mt-3
         '>
             {OptionList.map((item,index)=>(
-            <div key={index} className={`relative grayscale hover:grayscale-0 cursor-pointer p-1
-            ${selectedOption==item.label?'grayscale-0':'grayscale'}
-            `} onClick={()=>setSelectOption(item.label)}>
+            <div 
+                key={index} 
+                className={`relative grayscale hover:grayscale-0 cursor-pointer p-1 transition-transform duration-200
+                ${selectedOption==item.label?'grayscale-0 border-4 shadow-2xl rounded-3xl border-primary':' grayscale'}
+                `} onClick={()=>setSelectOption(item.label)}>
+                    
                 <h2 className='absolute bottom-4 text-2xl text-white text-center w-full'>{item.label}</h2>
-                <Image src={item.imageUrl} alt={item.label}
-                width={300}
-                height={500}
-                className='object-cover h-[260px] rounded-3xl'/>
+                <Image 
+                    src={item.imageUrl} 
+                    alt={item.label}
+                    width={300}
+                    height={500}
+                    className='object-cover h-[260px] rounded-3xl'/>
             </div>
                 ))}
         </div>
