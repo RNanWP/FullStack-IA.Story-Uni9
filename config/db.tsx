b3550f562454f4fc86386c807d17325cfff46602
RNanWP <renan92011@hotmail.com>
2024-11-03 16:51:03 -0300

    Adicionando Database postgre

---
+++ config/db.tsx
@@ -0,0 +1,4 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL!);
+export const db = drizzle({ client: sql });
